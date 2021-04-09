896db095eb1c13a3f9a4d041b2fd98ec410f3b33
jchuahtacc <jchuah@tacc.utexas.edu>
2021-04-09 13:28:49 -0500

    Meta

---
+++ gen/meta/src/apis/index.ts
@@ -0,0 +1,7 @@
+export * from './AggregationApi';
+export * from './CollectionApi';
+export * from './DbApi';
+export * from './DocumentApi';
+export * from './GeneralApi';
+export * from './IndexApi';
+export * from './RootApi';
