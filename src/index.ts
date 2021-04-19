15daca895edd88c59a919903cd658d324cf4aa89
jchuahtacc <jchuah@tacc.utexas.edu>
2021-04-19 10:19:33 -0500

    Builds top level package

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import * as Actors from '@tapis/tapis-typescript-actors';
+import * as Apps from '@tapis/tapis-typescript-apps';
+import * as Authenticator from '@tapis/tapis-typescript-authenticator';
+import * as Files from '@tapis/tapis-typescript-files';
+import * as Jobs from '@tapis/tapis-typescript-jobs';
+import * as Meta from '@tapis/tapis-typescript-meta';
+import * as PGRest from '@tapis/tapis-typescript-pgrest';
+import * as SK from '@tapis/tapis-typescript-sk';
+import * as Streams from '@tapis/tapis-typescript-streams';
+import * as Tenants from '@tapis/tapis-typescript-tenants';
+import * as Tokens from '@tapis/tapis-typescript-tokens';
+
+export {
+  Actors,
+  Apps,
+  Authenticator,
+  Files,
+  Jobs,
+  Meta,
+  PGRest,
+  SK,
+  Streams,
+  Tenants,
+  Tokens
+}
