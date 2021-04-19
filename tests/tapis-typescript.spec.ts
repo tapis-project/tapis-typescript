a1b11de189cd537731e633b576a5a69f0f2e2621
jchuahtacc <jchuah@tacc.utexas.edu>
2021-04-19 10:39:24 -0500

    Configure tests

---
+++ tests/tapis-typescript.spec.ts
@@ -0,0 +1,8 @@
+import { Actors } from '../src';
+import { expect } from 'chai';
+
+describe('tapis-typescript', () => { 
+  it('should have Actors service', () => {
+    expect(Actors).to.have.property('ActorsApi');
+  });
+});
