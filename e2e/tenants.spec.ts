56b3281eb5b6410ec103891b88b36f51b9f31e5b
jchuahtacc <jchuah@tacc.utexas.edu>
2021-04-26 13:32:16 -0500

    Tenants e2e tests

--- e2e/tenants.spec.ts
+++ e2e/tenants.spec.ts
@@ -17,7 +17,18 @@ describe('Tenants e2e tests', () => {
     const configuration: Tenants.Configuration = new Tenants.Configuration(configurationParameters);
     const api: Tenants.SitesApi = new Tenants.SitesApi(configuration);
     const listSitesRequest: Tenants.ListSitesRequest = {};
-    const response = await api.listSites(listSitesRequest);
-    expect(response).to.have.property("result");
+    const response: Tenants.RespListSites = await api.listSites(listSitesRequest);
+    expect(response.result.length).to.be.greaterThan(0);
+  });
+  it('should retrieve a list of tenants', async () => {
+    const configurationParameters: Tenants.ConfigurationParameters = {
+      basePath: process.env.TACC_TENANT,
+      fetchApi: fetch
+    }
+    const configuration: Tenants.Configuration = new Tenants.Configuration(configurationParameters);
+    const api: Tenants.TenantsApi = new Tenants.TenantsApi(configuration);
+    const listTenantsRequest: Tenants.ListTenantsRequest = {}
+    const response: Tenants.RespListTenants = await api.listTenants(listTenantsRequest);
+    expect(response.result.length).to.be.greaterThan(0);
   });
 });
