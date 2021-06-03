9de9e42edc53e93c3df24e432b63f3ecef4c692f
jchuahtacc <jchuah@tacc.utexas.edu>
2021-06-03 13:20:14 -0500

    Add Files to tapis-typescript exports

--- tapis-typescript/src/index.ts
+++ tapis-typescript/src/index.ts
@@ -1,11 +1,11 @@
 import * as Authenticator from '@tapis/tapis-typescript-authenticator';
 import * as Systems from '@tapis/tapis-typescript-systems';
 import * as Tenants from '@tapis/tapis-typescript-tenants';
+import * as Files from '@tapis/tapis-typescript-files';
 
 /*
 import * as Actors from '@tapis/tapis-typescript-actors';
 import * as Apps from '@tapis/tapis-typescript-apps';
-import * as Files from '@tapis/tapis-typescript-files';
 import * as Jobs from '@tapis/tapis-typescript-jobs';
 import * as Meta from '@tapis/tapis-typescript-meta';
 import * as PGRest from '@tapis/tapis-typescript-pgrest';
@@ -17,11 +17,11 @@ import * as Tokens from '@tapis/tapis-typescript-tokens';
 export {
   Authenticator,
   Systems,
-  Tenants
+  Tenants,
+  Files
   /*
   Actors,
   Apps,
-  Files,
   Jobs,
   Meta,
   PGRest,
