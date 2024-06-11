dd5a4e1c1f064441253798baa749741b774d16ae
Maximiliano Osorio <maxiosorio@gmail.com>
2024-06-11 11:23:56 -0400

    fix: add tests

--- tapis-typescript/e2e/jobs.spec.ts
+++ tapis-typescript/e2e/jobs.spec.ts
@@ -103,7 +103,7 @@ describe("Jobs e2e tests", async () => {
       if (subscription === undefined) {
         throw new Error("Subscription URL is undefined");
       } else {
-        expect(subscription.url).to.not.be.undefined;
+        expect(subscription).to.not.be.undefined;
       }
     } catch (error) {
       console.error(error);
@@ -117,62 +117,56 @@ describe("Jobs e2e tests", async () => {
       const api: Jobs.SubscriptionsApi = new Jobs.SubscriptionsApi(
         configuration
       );
-      const subscriptions: Jobs.TapisSubscription[] | undefined =
-        await getSubscriptions(api, testJobUuid);
-      if (subscriptions === undefined) {
-        throw new Error("Subscriptions are undefined");
-      }
+      //The previous test should have created a subscription for the job so we should find it
+      const subscriptions: Jobs.TapisSubscription[] =
+        await getSubscriptionsByJobUuid(api, testJobUuid);
       const subscription = subscriptions.find(
         (subscription) => subscription.subjectFilter === testJobUuid
       );
       expect(subscription).to.not.be.undefined;
+      expect(subscription?.subjectFilter).to.equal(testJobUuid);
     } catch (error) {
       console.error(error);
       throw error;
     }
   });
 
-  //test subscription api = deleteSubscriptions
-  // it("should delete subscriptions for a job", async () => {
-  //   try {
-  //     const api: Jobs.SubscriptionsApi = new Jobs.SubscriptionsApi(
-  //       configuration
-  //     );
+  it.skip("should delete subscriptions for a job using subscriptionId", async () => {
+    try {
+      const api: Jobs.SubscriptionsApi = new Jobs.SubscriptionsApi(
+        configuration
+      );
+      //The previous test should have created a subscription for the job so we should find it
+      const subscriptions: Jobs.TapisSubscription[] =
+        await getSubscriptionsByJobUuid(api, testJobUuid);
+      const subscription = subscriptions.find(
+        (subscription) => subscription.subjectFilter === testJobUuid
+      );
+      expect(subscription).to.not.be.undefined;
+      if (subscription === undefined || subscription.uuid === undefined) {
+        throw new Error("Subscription to delete is undefined");
+      }
+      // Delete the subscription
+      const request: Jobs.DeleteSubscriptionsRequest = {
+        uuid: subscription?.uuid,
+      };
+      const response: Jobs.ResultChangeCount = await api.deleteSubscriptions(
+        request
+      );
 
-  //     const request: Jobs.DeleteSubscriptionsRequest = {
-  //       uuid: testJobUuid,
-  //     };
-  //     const response: Jobs.ResultChangeCount = await api.deleteSubscriptions(
-  //       request
-  //     );
-
-  //     // Get the status of the job. If the job wasn't running then the subscription will not be deleted
-  //     const jobApi: Jobs.JobsApi = new Jobs.JobsApi(configuration);
-  //     const statusRequest: Jobs.GetJobListRequest = {};
-  //     const statusResponse: Jobs.RespGetJobList = await jobApi.getJobList(
-  //       statusRequest
-  //     );
-  //     const jobs: Array<Jobs.JobListDTO> | undefined = statusResponse.result;
-  //     if (jobs === undefined) {
-  //       throw new Error("Jobs are undefined");
-  //     }
-  //     // Check if the job is still running
-  //     const job = jobs.find((job) => job.uuid === testJobUuid);
-  //     if (job === undefined) {
-  //       throw new Error("Job not found");
-  //     }
-
-  //     expect(jobs.length).to.be.greaterThanOrEqual(1);
-
-  //     console.info("Deleted subscriptions", response.changes);
-  //     expect(response.changes).to.be.greaterThanOrEqual(1);
-  //   } catch (error) {
-  //     console.error(error);
-  //     throw error;
-  //   }
-  // });
+      console.info("Deleted subscriptions", response.changes);
+      expect(response.changes).to.be.greaterThanOrEqual(1);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  });
 });
-async function getSubscriptions(api: Jobs.SubscriptionsApi, jobUuid: string) {
+
+async function getSubscriptionsByJobUuid(
+  api: Jobs.SubscriptionsApi,
+  jobUuid: string
+) {
   const request: Jobs.GetSubscriptionsRequest = {
     jobUuid: jobUuid,
   };
@@ -180,5 +174,8 @@ async function getSubscriptions(api: Jobs.SubscriptionsApi, jobUuid: string) {
     request
   );
   const subscriptions: Jobs.TapisSubscription[] | undefined = response.result;
+  if (subscriptions === undefined) {
+    throw new Error("Subscriptions are undefined");
+  }
   return subscriptions;
 }
