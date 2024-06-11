bc435eb0ecca4617c4d643864154592dda7e2bd0
Maximiliano Osorio <maxiosorio@gmail.com>
2024-06-11 10:21:43 -0400

    fix: change `in` to `Object.values`
    Test was not working - the operator `in` was not finding the element.
    I test using `Object.values` and it worked

--- tapis-typescript/e2e/jobs.spec.ts
+++ tapis-typescript/e2e/jobs.spec.ts
@@ -1,16 +1,17 @@
 // Load e2e environment variables into process.env
-require('dotenv').config({ path: 'e2e.env' });
+require("dotenv").config({ path: "e2e.env" });
 
-import 'mocha';
-import { before } from 'mocha';
-import { 
-  Jobs
-} from '../src';
-import { getToken, checkJsonError } from './utils';
-import { expect } from 'chai';
-import fetch from 'cross-fetch';
+import "mocha";
+import { before } from "mocha";
+import { Jobs } from "../src";
+import { getToken, checkJsonError } from "./utils";
+import { expect } from "chai";
+import fetch from "cross-fetch";
 
-describe('Jobs e2e tests', async () => {
+let testJobUuid: string;
+const jobStatusEnumStatus: string[] = Object.values(Jobs.JobStatusEnum);
+
+describe("Jobs e2e tests", async () => {
   let configuration: Jobs.Configuration;
 
   before(async () => {
@@ -19,48 +20,90 @@ describe('Jobs e2e tests', async () => {
     const configurationParameters: Jobs.ConfigurationParameters = {
       basePath: process.env.TEST_TENANT,
       headers: {
-        "X-Tapis-Token": token.access_token
+        "X-Tapis-Token": token.access_token,
       },
-      fetchApi: fetch
-    }
+      fetchApi: fetch,
+    };
     configuration = new Jobs.Configuration(configurationParameters);
   });
 
-  it('should submit a job', async() => {
+  it("should submit a job", async () => {
     try {
       const api: Jobs.JobsApi = new Jobs.JobsApi(configuration);
       const reqSubmitJob: Jobs.ReqSubmitJob = {
         name: process.env.TEST_JOB_NAME,
         appId: process.env.TEST_APP,
         appVersion: process.env.TEST_APP_VERSION,
-        execSystemId: process.env.TEST_SYSTEM_ID
-      }
+        execSystemId: process.env.TEST_SYSTEM_ID,
+      };
       const response: Jobs.RespSubmitJob = await api.submitJob({
-        reqSubmitJob
+        reqSubmitJob,
       });
       const job: Jobs.Job = response.result;
       console.info("Submitted job uuid", job.uuid);
       const statusReq: Jobs.GetJobStatusRequest = {
-        jobUuid: job.uuid
+        jobUuid: job.uuid,
       };
-      const statusResponse: Jobs.RespGetJobStatus = await api.getJobStatus(statusReq);
+      const statusResponse: Jobs.RespGetJobStatus = await api.getJobStatus(
+        statusReq
+      );
       const jobStatus: Jobs.JobStatusDisplay = statusResponse.result;
-      console.info("Job status", jobStatus);
-      expect(jobStatus.status in Jobs.JobStatusEnum).to.equal(true);
+      if (jobStatus.status === undefined) {
+        throw new Error("Job status is undefined");
+      }
+      expect(jobStatus.status).to.be.oneOf(jobStatusEnumStatus);
+      testJobUuid = job.uuid;
+      console.info("Test job uuid", testJobUuid);
     } catch (error) {
-      checkJsonError(error);
+      console.error(error);
+      throw error;
     }
   });
 
-  it('should get a job listing', async() => {
+  it("should get a job listing", async () => {
     try {
       const api: Jobs.JobsApi = new Jobs.JobsApi(configuration);
       const request: Jobs.GetJobListRequest = {};
       const response: Jobs.RespGetJobList = await api.getJobList(request);
       const jobs: Array<Jobs.JobListDTO> = response.result;
       expect(jobs.length).to.be.greaterThanOrEqual(1);
-    } catch(error) {
-      checkJsonError(error);
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
   });
+
+  //test subscription api
+  // it("should subscribe to a job", async () => {
+  //   try {
+  //     const api: Jobs.SubscriptionsApi = new Jobs.SubscriptionsApi(
+  //       configuration
+  //     );
+  //     const body: Jobs.ReqSubscribe = {
+  //       description: "Test subscription",
+  //       enabled: true,
+  //       eventCategoryFilter:
+  //         Jobs.ReqSubscribeEventCategoryFilterEnum.JobNewStatus,
+  //       deliveryTargets: [],
+  //       ttlminutes: 60,
+  //     };
+
+  //     const request: Jobs.SubscribeRequest = {
+  //       jobUuid: testJobUuid,
+  //       reqSubscribe: body,
+  //     };
+  //     const { result: subscription }: Jobs.RespResourceUrl =
+  //       await api.subscribe(request);
+  //     expect(subscription).to.not.be.undefined;
+  //     // if (subscription === undefined) {
+  //     //   throw new Error("Subscription URL is undefined");
+  //     // } else {
+  //     //   console.info("Subscription URL", subscription.url);
+  //     //   expect(subscription.url).to.not.be.undefined;
+  //     // }
+  //   } catch (error) {
+  //     console.error(error);
+  //     throw error;
+  //   }
+  // });
 });
